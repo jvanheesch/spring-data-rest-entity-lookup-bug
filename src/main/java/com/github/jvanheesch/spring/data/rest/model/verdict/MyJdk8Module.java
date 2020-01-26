2ab1604630bfb380f9fc036c5762d698ba762e55
jvanheesch <joris.vanheesch@ae.be>
2020-01-26 20:22:51 +0100

    wip

--- src/main/java/com/github/jvanheesch/spring/data/rest/model/verdict/MyJdk8Module.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/model/verdict/MyJdk8Module.java
@@ -1,38 +1,13 @@
 package com.github.jvanheesch.spring.data.rest.model.verdict;
 
-import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.datatype.jdk8.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
-public class MyJdk8Module extends Module
-{
+public class MyJdk8Module extends SimpleModule {
 
     @Override
     public void setupModule(SetupContext context) {
         context.addSerializers(new Jdk8Serializers());
         context.addDeserializers(new Jdk8Deserializers());
-        // And to fully support Optionals, need to modify type info:
         context.addTypeModifier(new Jdk8TypeModifier());
-    }
-
-    @Override
-    public Version version() {
-        return PackageVersion.VERSION;
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return this == o;
-    }
-
-    // TODO_JORIS rename even tho not necessary
-    @Override
-    public String getModuleName() {
-        return "Jdk8Module";
     }
 }
