eb90f008d6360e43ecfe791b2fbd2dc972559347
jvanheesch <joris.vanheesch@ae.be>
2020-01-26 23:55:02 +0100

    twtff

--- src/test/java/com/github/jvanheesch/spring/data/rest/Serialization2Test.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/Serialization2Test.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.ByteArrayOutputStream;
@@ -20,6 +21,7 @@ import java.io.ByteArrayOutputStream;
 class Serialization2Test {
     @Autowired
     private ObjectMapper objectMapper;
+    private ObjectMapper objectMapper2 = Jackson2ObjectMapperBuilder.json().build();
 
     // TODO_JORIS: het is fine dat cases 2 en 3 hetzelfde behavior hebben, denk ik.
     // immers: enkel de eerste komt voor, en die moet leidden tot null in json.
@@ -34,17 +36,17 @@ class Serialization2Test {
 
         JsonEncoding encoding = JsonEncoding.UTF8;
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        JsonGenerator generator = this.objectMapper.getFactory().createGenerator(baos, encoding);
+        JsonGenerator generator = this.objectMapper2.getFactory().createGenerator(baos, encoding);
 
-        ObjectWriter objectWriter = this.objectMapper.writer();
+        ObjectWriter objectWriter = this.objectMapper2.writer();
         objectWriter.writeValue(generator, verdictRecordOwner);
 
         byte[] bytes = baos.toByteArray();
         System.out.println(new String(bytes));
 
-        String ser = objectMapper.writeValueAsString(verdictRecordOwner);
+        String ser = this.objectMapper2.writeValueAsString(verdictRecordOwner);
 
-        VerdictRecordOwner deser = objectMapper.readValue(ser, VerdictRecordOwner.class);
+        VerdictRecordOwner deser = this.objectMapper2.readValue(ser, VerdictRecordOwner.class);
 
         System.out.println(ser);
     }
