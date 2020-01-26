0b48684daded7a47e2cf2f953f9c90c9dfd8c574
jvanheesch <joris.vanheesch@ae.be>
2020-01-26 23:58:42 +0100

    2 and 3 are broken -> TODO!!

--- src/main/java/com/github/jvanheesch/spring/data/rest/model/DebugController.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/model/DebugController.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.github.jvanheesch.spring.data.rest.model.verdict.jackson.VerdictRecordOwner;
 import com.github.jvanheesch.spring.data.rest.repo.VerdictRecordOwnerRepository;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +16,17 @@ import java.io.IOException;
 
 @RestController
 public class DebugController {
-    private final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper1;
+    private final ObjectMapper objectMapper2 = Jackson2ObjectMapperBuilder.json().build();
+    private final ObjectMapper objectMapper3;
     private final VerdictRecordOwnerRepository verdictRecordOwnerRepository;
+    private final RepositoryRestMvcConfiguration repositoryRestMvcConfiguration;
 
-    public DebugController(ObjectMapper objectMapper, VerdictRecordOwnerRepository verdictRecordOwnerRepository) {
-        this.objectMapper = objectMapper;
+    public DebugController(ObjectMapper objectMapper1, VerdictRecordOwnerRepository verdictRecordOwnerRepository, RepositoryRestMvcConfiguration repositoryRestMvcConfiguration) {
+        this.objectMapper1 = objectMapper1;
         this.verdictRecordOwnerRepository = verdictRecordOwnerRepository;
+        this.repositoryRestMvcConfiguration = repositoryRestMvcConfiguration;
+        this.objectMapper3 = repositoryRestMvcConfiguration.objectMapper();
     }
 
     @GetMapping("/debug")
@@ -28,9 +35,9 @@ public class DebugController {
 
         JsonEncoding encoding = JsonEncoding.UTF8;
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        JsonGenerator generator = this.objectMapper.getFactory().createGenerator(baos, encoding);
+        JsonGenerator generator = this.objectMapper1.getFactory().createGenerator(baos, encoding);
 
-        ObjectWriter objectWriter = this.objectMapper.writer();
+        ObjectWriter objectWriter = this.objectMapper1.writer();
         objectWriter.writeValue(generator, verdictRecordOwner);
 
         String string = new String(baos.toByteArray());
