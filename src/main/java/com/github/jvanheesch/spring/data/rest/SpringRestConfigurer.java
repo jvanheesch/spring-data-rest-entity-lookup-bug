656c7c1f26fdb4f15f2e46d5600ee1dbc73d3fc1
jvanheesch <joris.vanheesch@ae.be>
2020-01-26 18:31:27 +0100

    testing jackson

--- src/main/java/com/github/jvanheesch/spring/data/rest/SpringRestConfigurer.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/SpringRestConfigurer.java
@@ -1,6 +1,8 @@
 package com.github.jvanheesch.spring.data.rest;
 
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.github.jvanheesch.spring.data.rest.repo.AuthorRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
@@ -15,5 +17,10 @@ public class SpringRestConfigurer implements RepositoryRestConfigurer {
                         author -> String.valueOf(author.getId()),
                         (authorRepository, id) -> authorRepository.findById(Long.valueOf(id))
                 );
+    }
+
+    @Bean
+    public Jdk8Module jdk8Module() {
+        return new Jdk8Module();
     }
 }
