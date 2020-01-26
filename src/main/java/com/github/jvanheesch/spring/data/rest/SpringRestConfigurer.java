49d0159d548e67cee93c6c27f2a427e4bcfc2479
jvanheesch <joris.vanheesch@ae.be>
2020-01-26 19:17:14 +0100

    wippp!

--- src/main/java/com/github/jvanheesch/spring/data/rest/SpringRestConfigurer.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/SpringRestConfigurer.java
@@ -1,11 +1,24 @@
 package com.github.jvanheesch.spring.data.rest;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleDeserializers;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.module.SimpleSerializers;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.github.jvanheesch.spring.data.rest.model.verdict.Verdict;
 import com.github.jvanheesch.spring.data.rest.repo.AuthorRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class SpringRestConfigurer implements RepositoryRestConfigurer {
@@ -23,5 +36,56 @@ public class SpringRestConfigurer implements RepositoryRestConfigurer {
     @Bean
     public Jdk8Module jdk8Module() {
         return new Jdk8Module();
+    }
+
+
+    /**
+     * https://github.com/spring-projects/spring-data-rest/blob/master/src/main/asciidoc/custom-jackson-deserialization.adoc
+     */
+    @Bean
+    public SimpleModule customSerializationOptionsModule() {
+        return new CustomSerializationOptionsModule();
+    }
+
+    public static class CustomSerializationOptionsModule extends SimpleModule {
+        @Override
+        public void setupModule(SetupContext context) {
+            SimpleSerializers serializers = new SimpleSerializers();
+            SimpleDeserializers deserializers = new SimpleDeserializers();
+
+            serializers.addSerializer(Verdict.class, new VerdictSerializer());
+            deserializers.addDeserializer(Verdict.class, new VerdictDeserializer());
+
+            context.addSerializers(serializers);
+            context.addDeserializers(deserializers);
+        }
+
+        // TODO_JORIS: fix @JsonUnwrapped shit.
+        public static class VerdictSerializer extends JsonSerializer<Verdict> {
+            @Override
+            public void serialize(Verdict value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                if (value != null) {
+                    gen.writeNumber(1L);
+                } else {
+                    gen.writeNull();
+                }
+            }
+        }
+
+        public static class VerdictDeserializer extends StdDeserializer<Verdict> {
+
+            public VerdictDeserializer() {
+                super(Verdict.class);
+            }
+
+            @Override
+            public Verdict deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+                JsonNode node = jp.getCodec().readTree(jp);
+                Long verdictId = node.longValue();
+
+                Verdict verdict = new Verdict();
+                return verdict;
+            }
+        }
     }
 }
