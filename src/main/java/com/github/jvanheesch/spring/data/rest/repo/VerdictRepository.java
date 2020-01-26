d4b62934635915cc26e8f11b33589adf737c2ca4
jvanheesch <joris.vanheesch@ae.be>
2020-01-26 19:14:00 +0100

    ip

--- src/main/java/com/github/jvanheesch/spring/data/rest/repo/VerdictRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.jvanheesch.spring.data.rest.repo;
-
-import com.github.jvanheesch.spring.data.rest.model.verdict.Verdict;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VerdictRepository extends CrudRepository<Verdict, Long> {
-}
