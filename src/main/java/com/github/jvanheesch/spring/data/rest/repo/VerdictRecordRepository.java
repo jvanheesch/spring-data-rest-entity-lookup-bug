82efcde435aff06e357d71a45b3d126139a7df75
jvanheesch <joris.vanheesch@ae.be>
2020-01-27 15:01:23 +0100

    see http://localhost:8080/verdictRecordOwners

--- src/main/java/com/github/jvanheesch/spring/data/rest/repo/VerdictRecordRepository.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/repo/VerdictRecordRepository.java
@@ -2,10 +2,10 @@ package com.github.jvanheesch.spring.data.rest.repo;
 
 import com.github.jvanheesch.spring.data.rest.model.verdict.jackson.VerdictRecord;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
+@Repository
 public interface VerdictRecordRepository extends CrudRepository<VerdictRecord, Long> {
     @RestResource
     @Override
