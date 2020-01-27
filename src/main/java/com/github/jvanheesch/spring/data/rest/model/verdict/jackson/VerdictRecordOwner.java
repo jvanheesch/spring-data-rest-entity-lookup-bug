1e8476249f56880b4754d11c32b52bea4b750b19
jvanheesch <joris.vanheesch@ae.be>
2020-01-27 16:32:36 +0100

    cleanup

--- src/main/java/com/github/jvanheesch/spring/data/rest/model/verdict/jackson/VerdictRecordOwner.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/model/verdict/jackson/VerdictRecordOwner.java
@@ -4,18 +4,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 
 @Entity
 public class VerdictRecordOwner {
     @Id
     private Long id;
-    @Transient
-    private VerdictRecord verdictRecord1;
-    @Transient
-    private VerdictRecord verdictRecord2;
-    @Transient
-    private VerdictRecord verdictRecord3;
 
     public Long getId() {
         return id;
@@ -30,25 +23,13 @@ public class VerdictRecordOwner {
         return new VerdictRecord("abc");
     }
 
-    public void setVerdictRecord1(VerdictRecord verdictRecord) {
-        this.verdictRecord1 = verdictRecord;
-    }
-
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public VerdictRecord getVerdictRecord2() {
         return new VerdictRecord();
     }
 
-    public void setVerdictRecord2(VerdictRecord verdictRecord2) {
-        this.verdictRecord2 = verdictRecord2;
-    }
-
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public VerdictRecord getVerdictRecord3() {
         return null;
-    }
-
-    public void setVerdictRecord3(VerdictRecord verdictRecord3) {
-        this.verdictRecord3 = verdictRecord3;
     }
 }
