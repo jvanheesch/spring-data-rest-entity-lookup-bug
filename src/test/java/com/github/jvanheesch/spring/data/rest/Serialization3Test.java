3f568bcc5801b4e6380d899a7520bbe233faffb2
jvanheesch <joris.vanheesch@ae.be>
2020-01-27 09:35:35 +0100

    wip!

---
+++ src/test/java/com/github/jvanheesch/spring/data/rest/Serialization3Test.java
@@ -0,0 +1,39 @@
+package com.github.jvanheesch.spring.data.rest;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.github.jvanheesch.spring.data.rest.model.verdict.jackson.VerdictRecordOwner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.ByteArrayOutputStream;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class Serialization3Test {
+    private ObjectMapper objectMapper1;
+    @Autowired
+    private RepositoryRestMvcConfiguration repositoryRestMvcConfiguration;
+
+    @Test
+    void testSerialization() throws Exception {
+        this.objectMapper1 = repositoryRestMvcConfiguration.halObjectMapper();
+        VerdictRecordOwner verdictRecordOwner = new VerdictRecordOwner();
+
+        JsonEncoding encoding = JsonEncoding.UTF8;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        JsonGenerator generator = this.objectMapper1.getFactory().createGenerator(baos, encoding);
+
+        ObjectWriter objectWriter = this.objectMapper1.writer();
+        objectWriter.writeValue(generator, verdictRecordOwner);
+
+        byte[] bytes = baos.toByteArray();
+        System.out.println(new String(bytes));
+    }
+}
