8504b842d915813812eed2ba86de5216c3e59bbf
jvanheesch <joris.vanheesch@ae.be>
2020-01-27 15:44:26 +0100

    Revert "boot"
    This reverts commit becada09

--- src/main/java/com/github/jvanheesch/spring/data/rest/model/verdict/Verdict.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/model/verdict/Verdict.java
@@ -1,12 +1,6 @@
 package com.github.jvanheesch.spring.data.rest.model.verdict;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 public class Verdict {
-    @Id
-    private Long id;
     private String value;
 
     public Verdict(String value) {
@@ -14,14 +8,6 @@ public class Verdict {
     }
 
     public Verdict() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getValue() {
--- src/main/java/com/github/jvanheesch/spring/data/rest/model/verdict/VerdictRecord.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/model/verdict/VerdictRecord.java
@@ -1,16 +1,6 @@
 package com.github.jvanheesch.spring.data.rest.model.verdict;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
-@Entity
 public class VerdictRecord {
-    @Id
-    private Long id;
-    // TODO_JORIS: lazy
-    @OneToOne(fetch = FetchType.EAGER)
     private Verdict value;
 
     public VerdictRecord(Verdict value) {
@@ -18,14 +8,6 @@ public class VerdictRecord {
     }
 
     public VerdictRecord() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public Verdict getValue() {
