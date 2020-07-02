d8ba130b33f96890cd25dd9c44f0e0ae9773efa3
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 13:14:51 +0200

    wip

---
+++ src/main/java/com/github/jvanheesch/spring/data/rest/BookService.java
@@ -0,0 +1,25 @@
+package com.github.jvanheesch.spring.data.rest;
+
+import com.github.jvanheesch.spring.data.rest.model.Book;
+import com.github.jvanheesch.spring.data.rest.repo.BookRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional
+    public Book save(Book book) {
+        Book saved = bookRepository.save(book);
+        if (book.getTitle().equals("exception")) {
+            throw new RuntimeException();
+        }
+        return saved;
+    }
+}
--- src/test/java/com/github/jvanheesch/spring/data/rest/ClassesTest.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/ClassesTest.java
@@ -1,15 +1,17 @@
 package com.github.jvanheesch.spring.data.rest;
 
+import com.github.jvanheesch.spring.data.rest.model.Book;
 import com.github.jvanheesch.spring.data.rest.pck.BookRepository2;
-import com.github.jvanheesch.spring.data.rest.pck.ServiceB;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.transaction.TestTransaction;
+
+import java.util.List;
 
 import static com.github.jvanheesch.spring.data.rest.ClassesTest.Ctx;
 
@@ -20,22 +22,34 @@ class ClassesTest {
 
     @Autowired
     private BookRepository2 bookRepository2;
+    @Autowired
+    private BookService bookService;
 //    @Autowired
 //    private ServiceB serviceB;
 
     @Test
     void whenBookAuthorGetterIsAnnotated_thenEntityLookupNoLongerWorks() {
-        System.out.println("");
+        try {
+            Book book = new Book();
+            book.setTitle("exception");
+            bookService.save(book);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        boolean flaggedForRollback = TestTransaction.isFlaggedForRollback();
+
+        List<Book> all = bookRepository2.findAll();
+        System.out.println(all.size());
     }
 
     @Configuration
     @ComponentScan(
-            basePackageClasses = BookRepository2.class,
-            includeFilters = @ComponentScan.Filter(
-                    type = FilterType.REGEX,
-                    pattern = "com\\.github\\.jvanheesch\\.spring\\.data\\.rest\\.pck\\.BookRepository2"
-            ),
-            useDefaultFilters = false
+            basePackageClasses = Application.class
+//            includeFilters = @ComponentScan.Filter(
+//                    type = FilterType.REGEX,
+//                    pattern = "com\\.github\\.jvanheesch\\.spring\\.data\\.rest\\.pck\\.BookRepository2"
+//            ),
+//            useDefaultFilters = false
     )
     static class Ctx {
     }
