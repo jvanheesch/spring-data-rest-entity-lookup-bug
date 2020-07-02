9a91ad6ca08864b5cd2098018aca9dc8bbfddbc1
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 18:11:40 +0200

    kaka

--- src/test/java/com/github/jvanheesch/spring/data/rest/mems/ClassesTest.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/mems/ClassesTest.java
@@ -17,6 +17,7 @@ import static com.github.jvanheesch.spring.data.rest.mems.ClassesTest.Ctx;
 // lol idiot, bypassing service etc.
 // TODO_JORIS: in huidige setup wordt db altijd proper opgekuist!
 // TODO: testen individueel lijken ok, dus t is een issue van unittestcleanup
+// @SpringBootTest heeft pcs rollback-on-exception, maar dan weer niet rollback-tss-tests ... ... TODO!
 @SpringBootTest
 @ContextConfiguration(classes = Ctx.class)
 class ClassesTest {
