15fb9c1a6dcbd86a51f22704bf7411edf2b067dd
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 12:49:36 +0200

    fkn end me already

--- src/test/java/com/github/jvanheesch/spring/data/rest/ClassesTest.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/ClassesTest.java
@@ -2,12 +2,10 @@ package com.github.jvanheesch.spring.data.rest;
 
 import com.github.jvanheesch.spring.data.rest.blah.Ctx;
 import com.github.jvanheesch.spring.data.rest.pck.BookRepository2;
-import com.github.jvanheesch.spring.data.rest.pck.ServiceB;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @DataJpaTest
