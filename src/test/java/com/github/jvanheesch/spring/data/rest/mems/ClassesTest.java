553e36e63e86479d8b58c372e07e7ea4c75326bc
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 15:49:02 +0200

    wip

--- src/test/java/com/github/jvanheesch/spring/data/rest/mems/ClassesTest.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/mems/ClassesTest.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 import static com.github.jvanheesch.spring.data.rest.mems.ClassesTest.Ctx;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest(classes = Ctx.class)
 class ClassesTest {
@@ -26,13 +27,11 @@ class ClassesTest {
 
     @Test
     void whenBookAuthorGetterIsAnnotated_thenEntityLookupNoLongerWorks() {
-        try {
+        assertThatThrownBy(() -> {
             Book book = new Book();
             book.setTitle("exception");
             bookService.save(book);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        }).isInstanceOf(Exception.class);
 
         // TODO_JORIS: hier zou ik dus 0 results verwachten ... !!
         List<Book> all = bookService.findAll();
