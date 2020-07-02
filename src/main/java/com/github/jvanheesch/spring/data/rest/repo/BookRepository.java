7e29d4f16fd75678fb7d7737e2789503bbb1805b
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 14:21:06 +0200

    wip

--- src/main/java/com/github/jvanheesch/spring/data/rest/repo/BookRepository.java
+++ src/main/java/com/github/jvanheesch/spring/data/rest/repo/BookRepository.java
@@ -2,14 +2,13 @@ package com.github.jvanheesch.spring.data.rest.repo;
 
 import com.github.jvanheesch.spring.data.rest.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    // @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     default Book saveInNewTransaction(Book book) {
         return save(book);
     }
