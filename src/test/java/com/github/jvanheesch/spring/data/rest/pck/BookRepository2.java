407ddc5908e81755aa6e628585a3f18c993cdf50
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 12:24:01 +0200

    kaput ...

--- src/test/java/com/github/jvanheesch/spring/data/rest/pck/BookRepository2.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/pck/BookRepository2.java
@@ -1,7 +1,9 @@
 package com.github.jvanheesch.spring.data.rest.pck;
 
+import com.github.jvanheesch.spring.data.rest.model.Book;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BookRepository2 {
+public interface BookRepository2 extends CrudRepository<Book, Long> {
 }
