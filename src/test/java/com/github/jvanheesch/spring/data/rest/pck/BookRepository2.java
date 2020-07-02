dcb499f2aae2b89fa7e73486eacc7bd3dda3b1bf
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 12:49:28 +0200

    fkn end me already

--- src/test/java/com/github/jvanheesch/spring/data/rest/pck/BookRepository2.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/pck/BookRepository2.java
@@ -1,9 +1,9 @@
 package com.github.jvanheesch.spring.data.rest.pck;
 
 import com.github.jvanheesch.spring.data.rest.model.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository2 extends CrudRepository<Book, Long> {
+public interface BookRepository2 extends JpaRepository<Book, Long> {
 }
