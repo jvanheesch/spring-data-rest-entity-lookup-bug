628d1b751e0bdae6ac50c1dddcd4a79c02dda798
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 12:08:35 +0200

    cleanup  - todo: regex!

--- src/test/java/com/github/jvanheesch/spring/data/rest/blah/Ctx.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/blah/Ctx.java
@@ -6,13 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-//@ComponentScan(basePackageClasses = ServiceA.class, excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, value = "com\\.example\\.ignore\\..*"))
 @ComponentScan(
         basePackageClasses = ServiceA.class,
-//        excludeFilters = @ComponentScan.Filter(
-//                type = FilterType.REGEX,
-//                pattern = "com\\.github\\.jvanheesch\\.spring\\.data\\.rest\\.blgah\\..*"
-//        )
         excludeFilters = @ComponentScan.Filter(
                 type = FilterType.ASSIGNABLE_TYPE,
                 value = ServiceA.class
