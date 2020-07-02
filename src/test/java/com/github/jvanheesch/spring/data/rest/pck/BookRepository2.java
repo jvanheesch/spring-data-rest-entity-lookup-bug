3563b666cd7f56aa9c2a5a091eb8f85bca8c8802
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 12:24:22 +0200

    Revert "kaput ..."
    This reverts commit 407ddc59

--- src/test/java/com/github/jvanheesch/spring/data/rest/pck/BookRepository2.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/pck/BookRepository2.java
@@ -1,9 +1,7 @@
 package com.github.jvanheesch.spring.data.rest.pck;
 
-import com.github.jvanheesch.spring.data.rest.model.Book;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface BookRepository2 extends CrudRepository<Book, Long> {
+public class BookRepository2 {
 }
