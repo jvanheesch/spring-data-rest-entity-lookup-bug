63d8735d736642307c6cf5c19ca5a545aef47fd3
jvanheesch <joris.vanheesch@ae.be>
2020-07-02 17:51:58 +0200

    mems

--- src/test/java/com/github/jvanheesch/spring/data/rest/mems/ClassesTest.java
+++ src/test/java/com/github/jvanheesch/spring/data/rest/mems/ClassesTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import static com.github.jvanheesch.spring.data.rest.mems.ClassesTest.Ctx;
 
+// TODO_JORIS: in huidige setup wordt db altijd proper opgekuist!
 // TODO: testen individueel lijken ok, dus t is een issue van unittestcleanup
 @DataJpaTest
 @ContextConfiguration(classes = Ctx.class)
